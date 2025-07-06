80d46c4976c83848935d6b40e17bc756c3aea3d6
Paul Joubert <pauljjoubert@gmail.com>
2025-07-06 19:08:17 +0200

    Use glob loader

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,14 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const sections = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/content/sections" }),
+
     schema: z.object({
         title: z.string(),
     }),
 });
 
 export const collections = {
-    posts: sections,
+    sections,
 };
